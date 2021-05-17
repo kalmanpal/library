d2b9d37dda6a025aab0dbe2a200febef5761a5af
kalmanpal <kalmanpal98@gamil.com>
2021-05-17 23:48:24 +0200

    delete reservations finally final

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -17,28 +17,28 @@ class ReservationController extends Controller
     function showReservations()
     {
         $data = DB::table('reservations')
-        ->join('books', 'reservations.isbn', "=", 'books.isbn')
-        ->join('users','reservations.email', "=", 'users.email')
-        ->orderBy('reservations.created_at', 'asc')->get();
+            ->join('books', 'reservations.isbn', "=", 'books.isbn')
+            ->join('users', 'reservations.email', "=", 'users.email')
+            ->orderBy('reservations.created_at', 'asc')->get();
         return view('employee/reservations', ['reservations' => $data]);
     }
 
     function showBooks()
     {
         $data = DB::table('stocks')->join('books', 'stocks.isbn', "=", 'books.isbn')
-        ->where('stocks.number', '>', 0)->get();
+            ->where('stocks.number', '>', 0)->get();
         return view('member/book_reservation ', ['books' => $data]);
     }
 
     function showMyReservations()
     {
         $data = DB::table('reservations')
-        ->join('stocks', 'reservations.isbn', "=", 'stocks.isbn')
-        ->join('books', 'stocks.isbn', "=", 'books.isbn')
-        ->join('users','reservations.email', "=", 'users.email')
-        ->where('reservations.email', '=', Auth::user()->email)
-        ->select('title', 'writer', 'reservations.isbn', 'year', 'expiry', 'reservations.id')
-        ->get();
+            ->join('stocks', 'reservations.isbn', "=", 'stocks.isbn')
+            ->join('books', 'stocks.isbn', "=", 'books.isbn')
+            ->join('users', 'reservations.email', "=", 'users.email')
+            ->where('reservations.email', '=', Auth::user()->email)
+            ->select('title', 'writer', 'reservations.isbn', 'year', 'expiry', 'reservations.id')
+            ->get();
         return view('member/myreservations', ['myreservations' => $data]);
     }
 
@@ -51,18 +51,27 @@ class ReservationController extends Controller
         $res->email = Auth::user()->email;
         $res->isbn = $stock->isbn;
         $res->save();
-        $stock->number = $stock->number-1;
+        $stock->number = $stock->number - 1;
         $stock->save();
         return redirect('/book_reservation');
     }
 
     function deleteReservation($id)
     {
-        $data = Reservation::find($id);
-        $data->delete();
+        $res = Reservation::find($id);
+
+        $seged = DB::table('stocks')
+            ->join('reservations', 'stocks.isbn', "=", 'reservations.isbn')
+            ->where('stocks.isbn', '=', $res->isbn)
+            ->get();
+
+        $number = $seged[0]->number;
+
+        $stock = DB::table('stocks')
+            ->where('stocks.isbn', $res->isbn)
+            ->update(['number' => $number + 1]);
+
+        $res->delete();
         return redirect('/myreservations');
     }
-
-
-
 }
